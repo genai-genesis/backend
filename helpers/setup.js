fca16289f9c99bdb4e6e19a8d411277802a7f9a0
pahu2353 <pahu2353@gmail.com>
2024-03-31 04:44:12 -0400

    PLS WORK

--- helpers/setup.js
+++ helpers/setup.js
@@ -2,10 +2,12 @@ const { setupMongo } = require("./db");
 const cloudinary = require("cloudinary").v2;
 const configureLibs = async () => {
   await setupMongo();
+
+  // pain pain pain pain pain
   cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    cloud_name: 'dy1ppnb6w',
     api_key: '759586126363323',
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+    api_secret: 'AkEQ0NdAsQ5ibDt9p9C9zD1rAPo',
   });
 };
 
